ccf4e194606a6015df701f8859f647d7b4167d69
Zhijie Xu <zhijie.xu@sjsu.edu>
2021-07-27 22:52:17 -0700

    changed order of the routes in songsRouter

--- router/songsRouter.js
+++ router/songsRouter.js
@@ -3,10 +3,10 @@ const router = Router();
 const songsMethods = require("../controllers/songsMethods");
 
 router.get("/", songsMethods.getAllSongs);
+router.get("/seed", songsMethods.seedSongs);
 router.get("/:id", songsMethods.getSong);
 router.delete("/:id", songsMethods.deleteSong);
 router.put("/:id", songsMethods.editSong);
 router.post("/", songsMethods.createSong);
-router.get("/seed", songsMethods.seedSongs);
 
 module.exports = router;
